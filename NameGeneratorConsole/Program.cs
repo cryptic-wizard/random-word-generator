0d3470008efc36d521392996ec32bc0648eaf145
impolitetulip <87053379+impolitetulip@users.noreply.github.com>
2021-07-07 23:05:34 -0500

    Added language files as embedded resources Added language and name format options TODO: Add format local member implementation

---
+++ NameGeneratorConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using NameGeneratorLibrary;
+
+namespace NameGeneratorConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NameGenerator nameGenerator = new NameGenerator();
+            NameGenerator.WordType[] format = { NameGenerator.WordType.adv, NameGenerator.WordType.adj, NameGenerator.WordType.noun };
+            List<string> names = nameGenerator.GetNames(format, 10);
+            string name = nameGenerator.GetName(format);
+
+            foreach(string s in names)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine(name);
+        }
+    }
+}
