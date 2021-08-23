a3a8a796f84c0a46214c30b1b53b892a3734d107
cryptic-wizard <87053379+cryptic-wizard@users.noreply.github.com>
2021-08-22 19:12:04 -0500

    #18 improve test reporting (#19)
    Added dorny test-reporter, updated README

--- RandomWordGeneratorConsole/Program.cs
+++ RandomWordGeneratorConsole/Program.cs
@@ -9,16 +9,14 @@ namespace RandomWordGeneratorConsole
         static void Main(string[] args)
         {
             WordGenerator wordGenerator = new WordGenerator();
-            WordGenerator.PartOfSpeech partOfSpeech = WordGenerator.PartOfSpeech.noun;
-            List<string> words = wordGenerator.GetWords(partOfSpeech, 10);
-            string word = wordGenerator.GetWord(partOfSpeech);
+            List<string> adj = wordGenerator.GetWords(WordGenerator.PartOfSpeech.adj, 10);
+            List<string> adj2 = wordGenerator.GetWords(WordGenerator.PartOfSpeech.adj, 10);
+            List<string> noun = wordGenerator.GetWords(WordGenerator.PartOfSpeech.noun, 10);
 
-            foreach(string s in words)
+            for(int i = 0; i < 10; i++)
             {
-                Console.WriteLine(s);
+                Console.WriteLine(adj[i] + ' ' + adj2[i] + ' ' + noun[i]);
             }
-
-            Console.WriteLine(word);
         }
     }
 }
